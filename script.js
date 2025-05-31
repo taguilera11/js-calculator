cd1b3e52043aa851cc69e810727f03ee7dcad8ce
Tomás Aguilera <t.aguilera@uniandes.edu.co>
2025-05-31 18:46:46 -0500

    Refactor script.js to use module imports and remove redundant code

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const calculatorDisplay = document.querySelector("h1");
-const inputBtns =  document.querySelectorAll("button");
-const clearBtn = document.getElementById("clear-btn");
-
-let firstValue = 0;
-let awaitingNextValue = false;
-let operatorValue = "";
+import { inputBtns, clearBtn } from "./src/selectors.js";
+import addDecimal from "./src/addDecimal.js";
+import useOperator from "./src/useOperator.js";
+import addNumberValue from "./src/addNumberValue.js";
+import { resetAll } from "./src/values.js";
 
 
 inputBtns.forEach((inputBtn) => {
@@ -16,67 +14,6 @@ inputBtns.forEach((inputBtn) => {
     else if (inputBtn.classList.contains("decimal")) {
         inputBtn.addEventListener("click", () => addDecimal());
     }
-});
-
-
-function addNumberValue(value) {
-    if (awaitingNextValue) {
-        calculatorDisplay.textContent = value;
-        awaitingNextValue = false;
-    } else {
-        const firstValue = calculatorDisplay.textContent;
-        calculatorDisplay.textContent = 
-            firstValue === "0" ? value : firstValue + value;
-    }
-}
-
-const calculate = {
-    "/" : (firstValue, secondValue) => firstValue / secondValue,
-    "*" : (firstValue, secondValue) => firstValue * secondValue,
-    "+" : (firstValue, secondValue) => firstValue + secondValue,
-    "-" : (firstValue, secondValue) => firstValue - secondValue,
-    "=" : (firstValue, secondValue) => secondValue
-};
-
-function useOperator(operator) {
-    const currentValue = Number(calculatorDisplay.textContent);
-    if (operatorValue && awaitingNextValue) {
-        operatorValue = operator;
-    }
-
-    else if(!operatorValue) {
-        firstValue = currentValue;
-    } else {
-        const calculation = calculate[operatorValue](firstValue, currentValue);
-        const calculationRounded = Number.isInteger(calculation) ? calculation : calculation.toFixed(2);
-        calculatorDisplay.textContent = calculationRounded;
-        firstValue = calculation;
-    }
-
-    awaitingNextValue = true;
-    operatorValue = operator;
-};
+}); 
 
 clearBtn.addEventListener("click", resetAll);
-
-function resetAll() {
-    if (firstValue !== 0 && !awaitingNextValue) {
-          awaitingNextValue = true;
-        calculatorDisplay.textContent = "0";
-    } else {
-        firstValue = 0;
-        awaitingNextValue = false;
-        operatorValue = "";
-        calculatorDisplay.textContent = "0";
-    }
-}
-
-function addDecimal() {
-    if (awaitingNextValue) {
-        return;
-    }
-    if (!calculatorDisplay.textContent.includes(".")) {
-        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
-    }
-        
-}
